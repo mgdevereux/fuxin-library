0af472ad5588a45155f0bbdc679d436b90e10369
lifuxin2001@gmail.com <lifuxin2001@gmail.com@cbd84f00-3333-c94c-7dee-22546e6af939>
2012-02-07 20:08:24 +0000

    Fixed a bug in EvalKernel that doesn't accept double-precision inputs.

--- trunk/randfeat/EvalKernel.m
+++ trunk/randfeat/EvalKernel.m
@@ -73,10 +73,10 @@ case 'cauchy'
     dist2 = a*ones(1,L2) + ones(L1,1)*b' - 2*samples1*samples2';
     K = kernelparam./(kernelparam + dist2);
 case 'chi2'
-    K = chi2_mex(samples1',samples2');
+    K = chi2_mex(single(samples1)',single(samples2)');
     K = (repmat(sum(samples1,2),1,L2)  + repmat(sum(samples2,2)',L1,1) - K) / 2;
 case 'exp_chi2'
-    K = chi2_mex(samples1',samples2');
+    K = chi2_mex(single(samples1)',single(samples2)');
     K = exp(- kernelparam * K);
 case 'chi2_skewed'
     K = chi2_mex_scinv(samples1',samples2',single(kernelparam(1))); % FIXME modified NIPS2010
