bce58deb1b8ff3de6cba4e5126a5774c9392b582
lifuxin2001@gmail.com <lifuxin2001@gmail.com@cbd84f00-3333-c94c-7dee-22546e6af939>
2013-03-20 22:05:50 +0000

    Several changes in randfeat to make it consistent

--- trunk/randfeat/rf_sample.m
+++ trunk/randfeat/rf_sample.m
@@ -5,7 +5,7 @@ function obj = rf_sample(obj)
 % Fuxin Li - fuxin.li@ins.uni-bonn.de
 % Catalin Ionescu - catalin.ionescu@ins.uni-bonn.de
 % Cristian Sminchisescu - cristian.sminchisescu@ins.uni-bonn.de
-
+if isfield(obj,'distribution') && ~isempty(obj.distribution)
 switch obj.distribution
   case 'gaussian'
     S = sqrt(obj.kernel_param) * sqrt(2) * randn(obj.dim, obj.Napp);
@@ -18,11 +18,23 @@ switch obj.distribution
     % for intersection kernel
     S = obj.kernel_param * tan(pi * (rand(obj.dim, obj.Napp) - 0.5));
   case 'period'
-    S = floor((2:(obj.Nperdim+1))/2);
+      if obj.Nperdim == 1
+          S = [];
+      else
+          try
+              var = load('V_vedaldi.mat');
+              S = floor((2:(200))/2);
+    % Change Napp to the correct form
+              obj.Napp = obj.dim * obj.Nperdim;
+              S = obj.period * repmat(S, [obj.dim 1]);
+          catch
+    S = floor((2:(obj.Nperdim))/2);
     % Change Napp to the correct form
     obj.Napp = obj.dim * (length(S) + 1);
     obj.Nperdim = length(S)+1;
     S = obj.period * repmat(S, [obj.dim 1]);
+          end
+      end
   case 'uniform'
     % Change Napp to the correct form
     S = obj.period * [0: 1/(obj.Nperdim-1) :1];% rand(1, obj.Nperdim)
@@ -31,23 +43,37 @@ switch obj.distribution
   case 'exp_chi2'
     switch obj.method
       case 'signals'
-        S = floor((2:(obj.Nperdim+1))/2);
-        obj.Nperdim = length(S)+1;
-        S = obj.period * repmat(S, [obj.dim 1]);
-%        S = obj.period *[-S S];
+      if obj.Nperdim == 1
+          S = [];
+      else
+          try
+              var = load('V_vedaldi.mat');
+              S = floor((2:(200))/2);
+    % Change Napp to the correct form
+              S = obj.period * repmat(S, [obj.dim 1]);
+          catch
+    S = floor((2:(obj.Nperdim))/2);
+    % Change Napp to the correct form
+    obj.Nperdim = length(S)+1;
+    S = obj.period * repmat(S, [obj.dim 1]);
+          end
+      end%        S = obj.period *[-S S];
         % Change Napp to the correct form
                 
       case 'sampling'
         error('Not implemented!');
-      case 'chebyshev'
+      case {'chebyshev', 'direct'}
         % No sampling
         S = [];
 %        obj.Nperdim = obj.Nperdim * 2 + 2;
     end
     % Always have omega2 for the second step Gaussian
+    obj.dim * obj.Nperdim
+    obj.Napp
     obj.omega2 = sqrt(obj.kernel_param) * randn(obj.dim*obj.Nperdim+1,obj.Napp);
   otherwise
     error('Unknown sampling distribution');
 end
 obj.omega = S;
+end
 end
